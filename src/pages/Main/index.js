9f6879a02900f356e4383526045604d309408ebb
Diogo <drd.rclrd@gmail.com>
2019-07-20 17:02:04 -0300

    was added method for new repositorie

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,30 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
 import { Container, Form, SubmitButton } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
+    repositories: []
   };
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
-    console.log(this.state.newRepo)
+    const { newRepo, repositories } = this.state;
+
+    const response = await axios.get(`https://api.github.com/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({ repositories: [ ...repositories, data], newRepo: '' })
   }
 
   render() {
