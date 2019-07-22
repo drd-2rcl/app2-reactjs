5495373a99990046c2e393a8fca61750cef9a182
Diogo <drd.rclrd@gmail.com>
2019-07-22 10:17:35 -0300

    new file for style in page repository

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
