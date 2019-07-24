1fb47b70867199768271d64a2fa91f8f71d0906b
Diogo <drd.rclrd@gmail.com>
2019-07-24 11:19:44 -0300

    :art: tags, a, img, h1, and paragraph

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -14,4 +14,30 @@ export const Owner = styled.header`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  a {
+    color: #7159c1;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 14px;
+    color: #666;
+    line-height: 1.4;
+    text-aligns: center;
+    max-width: 400px;
+  }
 `;
